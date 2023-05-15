ea50fac5fef0a02dce6bb73f1bbfd22e2cc2db04
Xepobopa <ismartsdn4477@gmail.com>
2023-05-15 15:46:13 +0300

    add NODE_ENV variable for swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,24 @@ import {Module} from '@nestjs/common';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {MongooseModule} from "@nestjs/mongoose";
 import * as Joi from "joi";
-import {UsersModule} from "./user/users.module";
+import {UserModule} from "./user/user.module";
 import {TokenModule} from "./token/token.module";
 import {AuthModule} from "./auth/auth.module";
+import {ProjectModule} from "./project/project.module";
 
 @Module({
     imports: [
-        UsersModule,
+        UserModule,
         TokenModule,
         AuthModule,
+        ProjectModule,
         ConfigModule.forRoot({
             validationSchema: Joi.object({
                 SECRET_ACCESS: Joi.string().required(),
                 SECRET_REFRESH: Joi.string().required(),
                 DB_CONNECTION_STRING: Joi.string().required(),
-                PORT: Joi.number().required()
+                PORT: Joi.number().required(),
+                NODE_ENV: Joi.string().required().valid("DEVELOPMENT", "PRODUCTION")
             })
         }),
         MongooseModule.forRootAsync({
